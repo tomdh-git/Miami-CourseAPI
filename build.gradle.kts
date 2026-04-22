1378d255b6497ef09c12ae1700c3bfb11098fa21
das-1102 <tomdh@miamioh.edu>
2026-04-22 01:46:44 -0400

    maybe

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
     testImplementation("com.netflix.graphql.dgs:graphql-dgs-spring-graphql-starter-test")
 
     // Math & Combinatorics
-    implementation("com.github.tomdh-git:interval-combinator:-c4280e8754-1")
+    implementation("com.github.tomdh-git:interval-combinator:master-SNAPSHOT")
 }
 
 tasks.withType<Test> {
