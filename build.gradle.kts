22b865a9967340e667296165ae6f6b35ec9f08f4
das-1102 <tomdh@miamioh.edu>
2026-04-22 12:25:27 -0400

    take out lint fr

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("jvm") version "2.0.21"
     kotlin("plugin.spring") version "2.0.21"
-    id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
 }
 
 group = "com.example"
